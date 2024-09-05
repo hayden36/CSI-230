35f4ce8021e826b69eb4398e37341137bce59b26
Hayden Clark <hayden.clark@mymail.champlain.edu>
2024-09-05 14:26:14 -0400

    Classwork 9/5

---
+++ week2/11.ps1
@@ -0,0 +1,3 @@
+﻿$files=Get-ChildItem -Recurse -Depth 0
+$files | Rename_item -NewName {$_.name -replace ".ps1", ".log"}
+Get-ChildItem
---
+++ week2/9.ps1
@@ -0,0 +1,6 @@
+﻿$folderpath="$PSScriptRoot\outfolder"
+if (Test-Path $folderpath) {
+    Write-Host "Already Exists"
+} else {
+    New-Item -Path $folderpath -ItemType Directory
+}
