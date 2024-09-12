a198811640c865ddc5160b0efd5b2c11a3e6d359
Hayden Clark <hayden.clark@mymail.champlain.edu>
2024-09-12 14:36:25 -0400

    Class 9/12

---
+++ week3/1-5.ps1
@@ -0,0 +1,83 @@
+﻿# Question 1
+Get-EventLog System -source Microsoft-Windows-Winlogon
+
+# Question 2/3
+$loginouts = Get-EventLog System -source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-14)
+$loginoutsTable = @()
+
+for ($i=0; $i -lt $loginouts.Count; $i++) {
+    $events = ""
+
+    if ($loginouts[$i].EventID -eq 7001) {$event="Logon"} 
+     if ($loginouts[$i].EventID -eq 7002) {$event="Logoff"} 
+     
+     $user = $loginouts[$i].ReplacementStrings[1]
+     $SID = New-Object System.Security.Principal.SecurityIdentifier($user)
+     $user = $SID.Translate([System.Security.Principal.NTAccount]).Value
+
+    
+
+     $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated; `
+                                            "Id" = $loginouts[$i].EventID; `
+                                             "Event" =$event; `
+                                            "User" = $user;
+                                            }
+}
+$loginoutsTable | Format-Table -AutoSize -Wrap
+
+# Question 4
+function Get-LoginLogoutEvents($daysToGet) {
+Write-Host "Getting login/logout events for the past $daysToGet day(s)."
+$loginouts = Get-EventLog System -source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-$daysToGet)
+$loginoutsTable = @()
+
+for ($i=0; $i -lt $loginouts.Count; $i++) {
+    $events = ""
+
+    if ($loginouts[$i].EventID -eq 7001) {$event="Logon"} 
+     if ($loginouts[$i].EventID -eq 7002) {$event="Logoff"} 
+     
+     $user = $loginouts[$i].ReplacementStrings[1]
+     $SID = New-Object System.Security.Principal.SecurityIdentifier($user)
+     $user = $SID.Translate([System.Security.Principal.NTAccount]).Value
+
+    
+
+     $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated; `
+                                            "Id" = $loginouts[$i].EventID; `
+                                             "Event" =$event; `
+                                            "User" = $user;
+                                            }
+}
+$loginoutsTable | Format-Table -AutoSize -Wrap
+}
+
+        
+
+Get-LoginLogoutEvents(2)
+
+
+
+# Question 5
+function Get-StartAndShutdownTimes($days) {
+Write-Host "Question 5: Getting start & shutdown events from the past $days day(s)."
+$loginouts = Get-EventLog System -After (Get-Date).AddDays(-$days)
+$loginoutsTable = @()
+
+for ($i=0; $i -lt $loginouts.Count; $i++) {
+    $events = ""
+
+    if ($loginouts[$i].EventID -eq 6005) {$event="Start"} 
+    elseif ($loginouts[$i].EventID -eq 6006) {$event="Shutdown"}
+    else {continue}
+     
+     $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated; `
+                                           "Id" = $loginouts[$i].EventID; `
+                                           "Event" = $event; `
+                                           "User" = "System";
+                                         }
+    }
+$loginoutsTable | Format-Table -AutoSize -Wrap
+}
+
+Get-StartAndShutdownTimes
---
+++ week3/6.ps1
@@ -0,0 +1,8 @@
+﻿. (Join-Path $PSScriptRoot 1-5.ps1)
+clear
+
+$loginoutsTable = Get-LoginLogoutEvents(15)
+$loginoutsTable
+
+$shutdownsTable = Get-StartAndShutdownTimes(25)
+$shutdownsTable
