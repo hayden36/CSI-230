df25c4d39bbc4f0d503991beb256aedc690022a8
Hayden Clark <hayden.clark@mymail.champlain.edu>
2024-09-26 14:49:30 -0400

    Finish classwork 9/26 for week5

---
+++ week5/HTMLElementsAndWebRequests.ps1
@@ -0,0 +1,25 @@
+﻿#9 (Deliverable 1).
+Write-Host "#9"
+$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://localhost/ToBeScraped.html
+
+$scraped_page.Links.Count
+
+#10 (Deliverable 2)
+Write-Host "#10"
+$scraped_page.Links
+
+#11 (Deliverable 3)
+Write-Host "#11"
+$scraped_page.Links | Select-Object outerText, href
+
+#12 (Deliverable 4)
+Write-Host "#12"
+$h2s = $scraped_page.ParsedHtml.body.getElementsByTagName("h2") | Select-Object outerText | Ft
+
+$h2s
+
+#13 (Deliverable 5)
+Write-Host "#13"
+$divs1 = $scraped_page.ParsedHtml.body.getElementsByTagName("div") | where { $_.getAttributeNode("class").Value -ilike "div-1"} | select innerText
+
+$divs1
