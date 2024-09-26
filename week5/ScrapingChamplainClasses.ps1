d48fc9afa73d4032049ee62f7e4058dd98578c9c
Hayden Clark <hayden.clark@mymail.champlain.edu>
2024-09-26 15:30:18 -0400

    Class 9/26

---
+++ week5/ScrapingChamplainClasses.ps1
@@ -0,0 +1,25 @@
+﻿function gatherClasses {
+    $page = Invoke-WebRequest -TimeoutSec 2 http://localhost/Courses.html
+
+    $trs = $page.ParsedHtml.body.getElementsByTagName("tr")
+
+    $FullTable = @()
+    for ($i=1; $i -lt $trs.length; $i++) {
+        $tds = $trs[$i].getElementsByTagName("td")
+
+        $Times = $tds[5].innerText.split("-")
+        
+        $FullTable += [pscustomobject]@{ `
+            "ClassCode" = $tds[0].innerHtml; `
+            "Title" = $tds[1].getElementsByTagName("a")[0].innerText; `
+            "Days" = $tds[4].innerHtml; `
+            "Time Start" = $Times[0]; `
+            "Time End" = $Times[1]; `
+            "Instructor" = $tds[6].getElementsByTagName("a")[0].innerText; `
+            "Location" = $tds[9].innerHtml; `
+         }   
+    }
+    return $FullTable
+}
+
+gatherClasses
