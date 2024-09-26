338d3f52431894eb0bc123d1be39ae771c10c3a5
Hayden Clark <hayden.clark@mymail.champlain.edu>
2024-09-26 13:51:34 -0400

    Finish Windows Apache Log assignment

--- week4/classwork.ps1
+++ week4/classwork.ps1
@@ -1,26 +1,20 @@
-﻿## 3. Get all logs
-#cls
-#Get-Content .\access.log
+﻿# 3. Get all logs
+Get-Content .\access.log
 
-##4.
-#cls
-#Get-Content .\access.log -Tail 5
+#4.
+Get-Content .\access.log -Tail 5
 
-##5. Display only 404s or 400s
-#cls
-#Get-Content .\access.log | Select-String -Pattern '404', '400'
+#5. Display only 404s or 400s
+Get-Content .\access.log | Select-String -Pattern '404', '400'
 
-##6. Only get NOT 200s
-#cls
-#Get-Content .\access.log | Select-String -Pattern '200' -NotMatch
+#6. Only get NOT 200s
+Get-Content .\access.log | Select-String -Pattern '200' -NotMatch
 
-##7. Only get logs that have the word "error"
-#cls
-#$A = Get-ChildItem .\*.log | Select-String -Pattern 'error'
-#$A[5..1]
+#7. Only get logs that have the word "error"
+$A = Get-ChildItem .\*.log | Select-String -Pattern 'error'
+$A[5..1]
 
 #8. Display only IP for 404s
-cls
 cd C:\xampp\apache\logs
 $notFounds = Get-Content .\access.log | Select-String -Pattern '404'
 
@@ -35,12 +29,10 @@ for ($i=0; $i -lt $ipsUnorg.Count; $i++) {
 #$ips | Where-Object {$_.IP -ilike "10.*"}
 
 #9. Get $ips from 8.
-cls
 $ipsoften = $ips | Where-Object {$_.IP -ilike "10.*"}
 $counts = $ipsoften | Group-Object IP
 $counts | Select-Object Count, Name
 
-
-# Parsing Logs Assignment
+# Functions
 . (Join-Path "C:\Users\champuser\CSI-230\week4" Apache-Logs.ps1)
 ApacheLogs1 -page "/" -httpCode 200 -browserName "Firefox"
