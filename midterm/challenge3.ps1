6e392c944b16b1683a053d126213e51e573dc97c
Hayden Clark <hayden.clark@mymail.champlain.edu>
2024-10-17 14:26:09 -0400

    Remove unused code

--- midterm/challenge3.ps1
+++ midterm/challenge3.ps1
@@ -7,22 +7,9 @@ $indicators = getIndicators
 $filteredLogs = @()
 
 # for each log, check if the page contains a `pattern` from the indicators
-# for ($i = 0; $i -lt $allLogs.Length; $i++) {
-#     for ($j = 0; $j -lt $indicators.Length; $j++) {
-#         Write-Host "looking for " $indicators[$j].Pattern
-
-#         $b = $indicators[$j].Pattern
-#         if ($b -ilike "*$allLogs[$i].Page*") {
-#             Write-Host "matched for" $b
-#             $filteredLogs += $allLogs[$i]
-#         }
-#     }
-# }
-
 for ($j = 0; $j -lt $indicators.Length; $j++) {
     $b = $indicators[$j]."Pattern"
     $filteredLogs += $allLogs | Where-Object { $_."Page" -ilike "*$b*" } 
-    # $filteredLogs += $allLogs | Where-Object { $_.Page -ilike "*.bash*" }
 }
 
 
