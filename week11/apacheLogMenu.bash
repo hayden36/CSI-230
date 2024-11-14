f95998a5604ce6dee4f3b2c2cf9ff6827b8e524f
Hayden Clark <hayden.clark@mymail.champlain.edu>
2024-11-13 22:50:00 -0500

    HW 11/13

--- week11/apacheLogMenu.bash
+++ week11/apacheLogMenu.bash
@@ -1,36 +1,39 @@
 #! /bin/bash
 
-logFile="/var/log/apache2/access.log.1"
+logFile="/var/log/apache2/access.log"
 
 function displayAllLogs(){
 	cat "$logFile"
 }
 
 function displayOnlyIPs(){
-        cat "$logFile" | cut -d ' ' -f 1 | sort -n | uniq -c
+	cat "$logFile" | cut -d ' ' -f 1 | sort -n | uniq -c
 }
 
 # function: displayOnlyPages:
 # like displayOnlyIPs - but only pages
+function displayOnlyPages() {
+	cat "$logFile" | cut -d ' ' -f 7 | sort -n | uniq -c
+}
 
 function histogram(){
 
 	local visitsPerDay=$(cat "$logFile" | cut -d " " -f 4,1 | tr -d '['  | sort \
-                              | uniq)
+		| uniq)
 	# This is for debugging, print here to see what it does to continue:
 	# echo "$visitsPerDay"
 
         :> newtemp.txt  # what :> does is in slides
-	echo "$visitsPerDay" | while read -r line;
-	do
-		local withoutHours=$(echo "$line" | cut -d " " -f 2 \
-                                     | cut -d ":" -f 1)
-		local IP=$(echo "$line" | cut -d  " " -f 1)
-          
-		local newLine="$IP $withoutHours"
-		echo "$IP $withoutHours" >> newtemp.txt
-	done 
-	cat "newtemp.txt" | sort -n | uniq -c
+        echo "$visitsPerDay" | while read -r line;
+        do
+        	local withoutHours=$(echo "$line" | cut -d " " -f 2 \
+        		| cut -d ":" -f 1)
+        	local IP=$(echo "$line" | cut -d  " " -f 1)
+
+        	local newLine="$IP $withoutHours"
+        	echo "$IP $withoutHours" >> newtemp.txt
+        done 
+        cat "newtemp.txt" | sort -n | uniq -c
 }
 
 # function: frequentVisitors: 
@@ -40,6 +43,10 @@ function histogram(){
 # number and check with a condition whether it is greater than 10
 # the output should be almost identical to histogram
 # only with daily number of visits that are greater than 10 
+
+function frequentVisitors() {
+	echo "$(displayOnlyIPs |  awk '$1 > 10')"
+}
 
 # function: suspiciousVisitors
 # Manually make a list of indicators of attack (ioc.txt)
@@ -51,15 +58,20 @@ function histogram(){
 # demonstrate loops, functions, etc. If you can do things simpler,
 # it is welcomed.
 
+function suspiciousVisitors() {
+	local iocList=$(displayAllLogs | egrep -i -f IOC.txt | cut -d " " -f 1 | sort -n | uniq -c )
+	echo "$iocList"
+}
+
 while :
 do
-	echo "PLease select an option:"
+	echo "Please select an option:"
 	echo "[1] Display all Logs"
 	echo "[2] Display only IPS"
-	# Display only pages visited
+	echo "[3] Display only pages visited"
 	echo "[4] Histogram"
-	# Frequent visitors
-	# Suspicious visitors
+	echo "[5] Frequent visitors"
+	echo "[6] Suspicious visitors"
 	echo "[7] Quit"
 
 	read userInput
@@ -77,15 +89,22 @@ do
 		echo "Displaying only IPS:"
 		displayOnlyIPs
 
-	# Display only pages visited
+	elif [[ "$userInput" == "3" ]]; then
+		echo "Displaying only pages visited:"
+		displayOnlyPages
 
 	elif [[ "$userInput" == "4" ]]; then
 		echo "Histogram:"
 		histogram
 
-        # Display frequent visitors
-	# Display suspicious visitors
-	# Display a message, if an invalid input is given
+	elif [[ "$userInput" == "5" ]]; then
+		echo "Frequent Visitors:"
+		frequentVisitors
+	elif [[ "$userInput" == "6" ]]; then
+		echo "Suspicious Visitors:"
+		suspiciousVisitors
+	else 
+		echo "Invalid option."
 	fi
 done
 
